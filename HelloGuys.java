29ce0a25355608983f1ab856017d51f541565175
annie24vijay <anniederose24@gmail.com>
2025-06-08 18:48:56 +0530

    Update HelloGuys.java

--- HelloGuys.java
+++ HelloGuys.java
@@ -5,6 +5,7 @@ public class HelloGuys {
                 System.out.println("Jesus Loves Me");
                 System.out.println("I am sorry Jesus")
                 System.out.println("Forgive me Jesus");
-                System.out.println("I am a sinner");//newly added
+                System.out.println("I am a sinner");
+                System.out.println("I am guilty")//newly added
         }
 }
